fb156ee956044adcfd79f3dd2c97872e89912fd7
Vidushi <107294109+vid-Math@users.noreply.github.com>
2024-05-15 17:26:56 +0530

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,87 @@
+import bs4
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_chroma import Chroma
+from langchain_community.chat_message_histories.in_memory import ChatMessageHistory
+# from langchain_community.document_loaders.web_base import WebBaseLoader
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+# from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders.unstructured import UnstructuredFileLoader
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+loader = UnstructuredFileLoader('schema_context.txt')
+
+docs = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(docs)
+vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+retriever = vectorstore.as_retriever()
+
+
+### Contextualize question ###
+contextualize_q_system_prompt = """Given a chat history and the latest user question \
+which might reference context in the chat history, formulate a standalone question \
+which can be understood without the chat history. Do NOT answer the question, \
+just reformulate it if needed and otherwise return it as is."""
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+history_aware_retriever = create_history_aware_retriever(
+    llm, retriever, contextualize_q_prompt
+)
+
+
+### Answer question ###
+qa_system_prompt = """You are an agent designed to interact with a SQL database. You return queries to the user
+Given an input question, create a syntactically correct SQL query to run, 
+then look at the results of the query. 
+Unless the user specifies a specific number of examples they wish to obtain, 
+always limit your query to at most 10 results.
+You can order the results by a relevant column to return the most interesting examples 
+in the database.
+Never query for all the columns from a specific table, only ask for the relevant columns 
+given the question.
+You MUST double check your query before executing it. 
+If you get an error while executing a query, rewrite the query and try again.
+
+Always return the final sql query and results of the query.
+
+DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
+
+Use the following pieces of retrieved context to answer the question.
+{context}
+"""
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", qa_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+
+question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+
+### Statefully manage chat history ###
+store = {}
+
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
